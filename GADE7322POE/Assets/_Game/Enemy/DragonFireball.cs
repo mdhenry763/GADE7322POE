d4aac1b5e7ff3b41bbaadad715b2619f283ccd2f
Michael Henry <mdhenry763@gmail.com>
2024-10-14 14:25:30 +0200

    dragons are now firing with an offset.

--- GADE7322POE/Assets/_Game/Enemy/DragonFireball.cs
+++ GADE7322POE/Assets/_Game/Enemy/DragonFireball.cs
@@ -7,11 +7,12 @@ public class DragonFireball : MonoBehaviour
 {
     [SerializeField] private int damageAmt = 5;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 offset = new Vector3(0, 0.5f, 0);
     [SerializeField] private float force = 5;
 
     public void InitializeFireball(Vector3 direction)
     {
-        rb.velocity = direction * force;
+        rb.velocity = (direction + offset) * force;
     }
     
     
